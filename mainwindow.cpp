87a3bfcbcc26853e6c356be58f897cf85ed95fb8
Aleksey Papin <gymlyg@rambler.ru>
2022-01-20 22:37:55 +0300

    UI: Table columns tuning

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -19,6 +19,17 @@ bool MainWindow::init()
     m_pTrackSqlModel = new TrackSqlModel();
     m_pTrackSqlModel->selQuery();
     ui->tableView_day_tracks->setModel(m_pTrackSqlModel);
+
+    m_pTrackSqlModel->setHeaderData(1, Qt::Horizontal, tr("col 1"));
+    m_pTrackSqlModel->setHeaderData(2, Qt::Horizontal, tr("col 2"));
+    m_pTrackSqlModel->setHeaderData(3, Qt::Horizontal, tr("col 3"));
+    m_pTrackSqlModel->setHeaderData(4, Qt::Horizontal, tr("col 4"));
+
+    ui->tableView_day_tracks->hideColumn(0);
+    ui->tableView_day_tracks->horizontalHeader()->setStretchLastSection(true);
+    ui->tableView_day_tracks->horizontalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
+    ui->tableView_day_tracks->resizeColumnToContents(true);
+
     ui->tableView_day_tracks->show();
     return true;
 }
